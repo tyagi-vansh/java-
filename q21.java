bf9870b9f1b7be6fd298509504cf03f95f1643be
vansh tyagi <er.vanshtyagi@gmail.com>
2023-10-08 21:33:01 +0530

    character_count

--- q21.java
+++ q21.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
+
 public class q21 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter your string");
+        String s = sc.nextLine();
+        for(int i=0;i<s.length();i++)
+        {
+              int c=0;
+            char q=s.charAt(i);
+            for(int j=0;j<s.length();j++){
+                char w=s.charAt(j);
+                if(q==w){
+                    c++;
+                }
+            }
+            System.out.println("the "+q+" is present ="+c+"times");
+          }
+          sc.close();
     }
     
 }
